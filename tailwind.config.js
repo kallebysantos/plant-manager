aefc4b09ae86cf0f94f7760476526aeeae936215
kalleby santos <kalleby_santos@hotmail.com>
2023-02-22 17:33:41 +0000

    📦 Customizing theme: Fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./App.tsx', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          100: '#EBF6FF',
+          300: '#3D7199',
+        },
+        red: {
+          500: '#E83F5B',
+        },
+        green: {
+          100: '#DAF2E4',
+          500: '#32B768',
+          700: '#2B7A4B',
+        },
+      },
+      fontFamily: {
+        heading: ['Jost-SemiBold'],
+        body: ['Jost-Regular'],
+      },
+    },
   },
   plugins: [],
 };
