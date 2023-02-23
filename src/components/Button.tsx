aec040f4fee2e62fd0cd6be1ea05ef365729cd90
kalleby santos <kalleby_santos@hotmail.com>
2023-02-23 11:47:17 +0000

    👌 IMPROVE: Button component extending Tailwindcss

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import {Text, TouchableOpacity, TouchableOpacityProps} from 'react-native';
+import {StyledComponent} from 'nativewind';
+import {StyledProps} from 'nativewind/dist/styled';
 
-type ButtonProps = TouchableOpacityProps & {
+type ButtonProps = StyledProps<Omit<TouchableOpacityProps, 'children'>> & {
   title: string;
 };
 
 export function Button({title, ...props}: ButtonProps) {
   return (
-    <TouchableOpacity
-      className="h-14 items-center justify-center rounded-lg bg-green-500"
-      activeOpacity={0.7}
-      {...props}>
-      <Text className="text-base text-white  font-heading">{title}</Text>
-    </TouchableOpacity>
+    <StyledComponent component={TouchableOpacity} {...props}>
+      <TouchableOpacity
+        className="h-14 items-center justify-center rounded-lg bg-green-500"
+        activeOpacity={0.7}
+        {...props}
+      >
+        <Text className="text-base text-white  font-heading">{title}</Text>
+      </TouchableOpacity>
+    </StyledComponent>
   );
 }
