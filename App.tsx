c9077e07be529bfd07491ae63311ea88b700c014
kalleby santos <kalleby_santos@hotmail.com>
2023-02-23 12:04:25 +0000

    👌 IMPROVE: StatusBar theme style

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import colors from 'tailwindcss/colors';
 
 import {Routes} from './src/routes';
 
 export default function App() {
+  const colorScheme = useColorScheme();
+
   return (
     <SafeAreaView className="flex-1">
+      <StatusBar
+        barStyle={colorScheme === 'light' ? 'dark-content' : 'light-content'}
+        backgroundColor={
+          colorScheme === 'light' ? colors.white : colors.gray[800]
+        }
+      />
       <Routes />
     </SafeAreaView>
   );
